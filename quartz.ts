0c97056917bccb80d77275884c8b281765abdd69
rawat-priyanshuu <147842205+priyanshu-rawa@users.noreply.github.com>
2026-08-06 15:35:02 +0530

    Update quartz.ts

--- quartz.ts
+++ quartz.ts
@@ -1,5 +1,11 @@
+import Lenis from "./quartz/components/Lenis"
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
 
 const config = await loadQuartzConfig()
 export default config
-export const layout = await loadQuartzLayout()
+
+export const layout = await loadQuartzLayout({
+  defaults: {
+    afterBody: [Lenis()],
+  },
+})
